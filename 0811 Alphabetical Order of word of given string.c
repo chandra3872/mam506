7b590bc70d20857f498633401646f3c04d91a3ec
chandra3872 <chandraketu2203872@dei.ac.in>
2024-11-08 16:09:04 +0530

    Create 0811 Alphabetical Order of word of given string.c

---
+++ 0811 Alphabetical Order of word of given string.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+void sortString(char *str) {
+    int len = strlen(str);
+    for (int i = 0; i < len - 1; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if (str[i] > str[j]) {
+                char temp = str[i];
+                str[i] = str[j];
+                str[j] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%s", str);
+    sortString(str);
+    printf("Sorted string: %s\n", str);
+    return 0;
+}
