c88dc7c03b0ab67afd45948dd6fce1203418bdfd
chandra3872 <chandraketu2203872@dei.ac.in>
2024-11-08 15:56:36 +0530

    Update series.c

--- series.c
+++ series.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 void main() {
-	int n, a=0, b=1, c=0;
+	int n,a=0,b=1,c=1,d=1,p=0;
 	printf("enter the value of n: ");
 	scanf("%d", &n);
 	while(a<n) {
-		c=b+c;
+		d=-d;
+		c=p+c;
 		b=b+2;
+		p=d*b;
 		a=a+1;
 	}
-	printf("%d\n", c);
+	printf("%d\n",c);
 }
